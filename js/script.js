3f0e428f437a94456a6baf08e2e53f51cb29a771
Salem aka lece <90127911+Onlylece@users.noreply.github.com>
2021-10-11 02:18:13 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+<script type="module" src="/js/js.cookie.mjs"></script>
+<script nomodule defer src="/js/js.cookie.js"></script>
+
+<script type="module" src="/js/js.cookie.mjs"></script>
+<script type="module">
+  import Cookies from '/js/js.cookie.mjs'
+
+  Cookies.set('foo', 'bar')
+</script>
