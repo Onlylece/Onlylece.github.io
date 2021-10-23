82cbf8c8d7c6db3c3835f018d296838559eb6ee4
Salem aka lece <90127911+Onlylece@users.noreply.github.com>
2021-10-23 17:51:16 +0400

    Update id.php

--- id.php
+++ id.php
@@ -1,3 +1,7 @@
-<a href="id.php?id=<?php echo $ido;?>"STYLE="TEXT-DECORATION: NONE"><?php echo $nume;?></a>
-
-$id = $_GET['1'];
+if( is_page(96) || is_page(98) || is_page(61) ) : ?> 
+    <div class="test"><h2>test content</h2></div>
+ <?php endif;
+$id = get_the_ID();
+if( ($id == 96) || ($id == 98) || ($id == 61) ) : ?> 
+        <div class="test"><h2>test content</h2></div>
+     <?php endif;
